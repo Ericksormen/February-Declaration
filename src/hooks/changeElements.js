9fbb85695e941587a36be944b49b211b3fd99ae2
Ericksormen <156144037+Ericksormen@users.noreply.github.com>
2024-02-12 21:54:34 -0600

    Update changeElements.js

--- src/hooks/changeElements.js
+++ src/hooks/changeElements.js
@@ -4,7 +4,7 @@ import confetti from 'canvas-confetti'
 export const useChangeElements = () => {
     const [index, setIndex] = useState(0)
     const [textP, setTextP] = useState('Vamos, respondeme! ')
-    const [image, setImage] = useState('https://imgur.com/a/idkPLR0')
+    const [image, setImage] = useState('https://tenor.com/es-US/view/wink-lightning-mcqueen-cars-smile-if-you-catch-my-drift-gif-26641275')
 
     const options = ['Segura?', 'Segurisima?', 'Estas completamente segura?', 'No te arrepentiras?', 'Pero si estas segura?', 'No hay vuelta atras', 'No hay devoluciones', 'No hay garantias', 'No hay reembolsos', 'No hay cambios', 'No hay nada', 'No hay', 'Que no hay']
 
